f0db56de4246e3b9217997e0aa63dd32ba7b92fb
mrmorcs <morcs@morcs.com>
2017-07-29 10:06:48 +0100

    Start building lable function (incomplete)

--- main.hs
+++ main.hs
@@ -1,1 +1,13 @@
-main = putStrLn "Hello"
+data Tr a = Lf a | Br (Tr a) (Tr a)
+    deriving Show
+
+type Lt a = Tr (S, a)
+type S = Int
+
+label :: Tr a -> Lt a
+label tr = snd (lab tr 0)
+    where
+        lab :: Tr a -> S -> (S, Lt a)
+        lab (Lf contents) n = (n+1, Lf (n, contents))
+
+tr1 = Br (Lf 'a') (Br (Br (Lf 'b') (Lf 'a')) (Lf 'c'))
